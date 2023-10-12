added kl + fixed kl

src/experiments/repeat.py
@@ -16,7 +16,8 @@ import json
 import matplotlib.pyplot as plt
 #sys.path.append('../src')
 from .utils.curvature_utils import compute_global_curvature, early_decode, layer_wise_curvature, \
-                                    logits_to_ave_logit_diff, residual_stack_to_logit_diff
+                                    logits_to_ave_logit_diff, residual_stack_to_logit_diff, \
+                                    layer_wise_norm_running_average
 from .utils.plotting_functions import plot_curvature_loss_vs_repetitions, plot_curvature_vs_repetitions, \
                                        plot_rr_vs_repetitions, plot_loss_vs_repetitions, plot_clusters, \
                                        plot_layer_curvature_loss_vs_repetitions, plot_logit_lens
@@ -136,7 +137,10 @@ def repeat_sequence_w_space(num_examples, string_sequence):
   return ' '+s.strip()
 
 # --------------------------------------------------------------ANALYSIS FUNCTIONS-----------------------------------------------------#
-def repeated_sequence_analysis(string_sequence, num_repeats, model, with_space=False):
+
+from tqdm import tqdm
+
+def repeated_sequence_analysis(string_sequence, num_repeats, model, with_space=False, compute_global=True, norm=False):
   '''Analyzes the curvature of a repeated sequence.
   Params:
       string_sequence: str, the string sequence to repeat
@@ -165,7 +169,14 @@ def repeated_sequence_analysis(string_sequence, num_repeats, model, with_space=F
     # print((stream_index + 1)/seq_length)
     losses = loss[stream_index::seq_length]
     logits = model_logits[0, stream_index::seq_length, :].cpu()
-    dataset_curvatures = compute_global_curvature(model_cache, model.cfg.n_layers, stream_index, seq_length)
+    if norm: 
+      dataset_curvatures = layer_wise_norm_running_average(model_cache, model.cfg.n_layers, stream_index, seq_length)
+    else:
+      if compute_global: 
+        dataset_curvatures = compute_global_curvature(model_cache, model.cfg.n_layers, stream_index, seq_length)
+      else: 
+        dataset_curvatures = layer_wise_curvature(model_cache, model.cfg.n_layers, stream_index, seq_length)
+      
     dataset_log_probs = torch.nn.functional.log_softmax(logits.cpu(), dim=-1)
     # answer_residual_directions = model.tokens_to_residual_directions(answer_tokens)
 
@@ -200,7 +211,8 @@ def repeated_sequence_analysis(string_sequence, num_repeats, model, with_space=F
 def repeated_sequence_sample_generation(num_samples, seq_len, 
                                         seq_position, num_repeats, 
                                         model, tokenizer, 
-                                        how='random', compute_global=True):
+                                        how='random', compute_global=True,
+                                        norm=False):
   
   sample_vectors = []
   sample_sequences = []
@@ -233,10 +245,13 @@ def repeated_sequence_sample_generation(num_samples, seq_len,
     for stream_index in range(seq_length):
       losses = loss[stream_index::seq_length]
       logits = model_logits[0, stream_index::seq_length, :].cpu()
-      if compute_global:
-        dataset_curvatures = compute_global_curvature(model_cache, model.cfg.n_layers, stream_index, seq_length)
+      if norm: 
+        dataset_curvatures = layer_wise_norm_running_average(model_cache, model.cfg.n_layers, stream_index, seq_length)
       else:
-        dataset_curvatures = layer_wise_curvature(model_cache, model.cfg.n_layers, stream_index, seq_length)
+        if compute_global:
+          dataset_curvatures = compute_global_curvature(model_cache, model.cfg.n_layers, stream_index, seq_length)
+        else:
+          dataset_curvatures = layer_wise_curvature(model_cache, model.cfg.n_layers, stream_index, seq_length)
       dataset_log_probs = torch.nn.functional.log_softmax(logits.cpu(), dim=-1)
       
       index_results = {
@@ -331,28 +346,31 @@ def main(FLAGS):
   num_layers = model.cfg.n_layers
   
   if FLAGS.experiment == "generate_streamlit_data":  
+    norm = True
     sample_vectors, sample_sequences, sample_losses = repeated_sequence_sample_generation(FLAGS.num_trials, FLAGS.seq_length, 
                                                                                           FLAGS.seq_length - 1, FLAGS.num_repetitions, 
                                                                                           model, tokenizer, how='random', 
-                                                                                          compute_global=False)
+                                                                                          compute_global=False, norm=norm)
 
     sample_vectors_p, sample_sequences_p, sample_losses_p = repeated_sequence_sample_generation(FLAGS.num_trials, FLAGS.seq_length, 
                                                                                                 FLAGS.seq_length - 1, FLAGS.num_repetitions, 
                                                                                                 model, tokenizer, how='pattern', 
-                                                                                                compute_global=False)
+                                                                                                compute_global=False, norm=norm)
     
     sample_vectors_t, sample_sequences_t, sample_losses_t = repeated_sequence_sample_generation(FLAGS.num_trials, FLAGS.seq_length, 
                                                                                                 FLAGS.seq_length - 1, FLAGS.num_repetitions, 
                                                                                                 model, tokenizer, how='top100', 
-                                                                                                compute_global=False)
+                                                                                                compute_global=False, norm=norm)
     
     sample_vectors_b, sample_sequences_b, sample_losses_b = repeated_sequence_sample_generation(FLAGS.num_trials, FLAGS.seq_length, 
                                                                                                 FLAGS.seq_length - 1, FLAGS.num_repetitions, 
                                                                                                 model, tokenizer, how='bottom100', 
-                                                                                                compute_global=False)
+                                                                                                compute_global=False, norm=norm)
     
     
-    keys_layers = list(range(num_layers)) + ['total'] 
+    keys_layers = list(range(num_layers))
+    if not norm:
+      keys_layers += ['total'] 
     ## total is the sum of the layers curvature (equivalent to the global curvature)
     
     layers_random = { str(layer):[[curv[layer] for curv in curvatures] for curvatures in sample_vectors]
@@ -372,14 +390,14 @@ def main(FLAGS):
     layers_bottom100['loss'] = [sample_loss.cpu().tolist() for sample_loss in sample_losses_b]
     
     os.makedirs(f"outputs/repeat/streamlit/", exist_ok=True)
-    pd.Series(sample_sequences).to_csv(f"outputs/repeat/streamlit/sequences_random.csv", header=False)
-    # pd.Series(sample_sequences_p).to_csv(f"outputs/repeat/streamlit/sequences_pattern.csv", header=False)
-    # pd.Series(sample_sequences_t).to_csv(f"outputs/repeat/streamlit/sequences_top100.csv", header=False)
-    pd.Series(sample_sequences_b).to_csv(f"outputs/repeat/streamlit/sequences_bottom100.csv", header=False)
-    json.dump(layers_random, open(f"outputs/repeat/streamlit/layers_loss_random.json", "w"))
-    # json.dump(layers_pattern, open(f"outputs/repeat/streamlit/layers_loss_pattern.json", "w"))
-    # json.dump(layers_top100, open(f"outputs/repeat/streamlit/layers_loss_top100.json", "w"))
-    json.dump(layers_bottom100, open(f"outputs/repeat/streamlit/layers_loss_bottom100.json", "w"))
+    pd.Series(sample_sequences).to_csv(f"outputs/repeat/streamlit/sequences_random_n.csv", header=False)
+    pd.Series(sample_sequences_p).to_csv(f"outputs/repeat/streamlit/sequences_pattern_n.csv", header=False)
+    pd.Series(sample_sequences_t).to_csv(f"outputs/repeat/streamlit/sequences_top100_n.csv", header=False)
+    pd.Series(sample_sequences_b).to_csv(f"outputs/repeat/streamlit/sequences_bottom100_n.csv", header=False)
+    json.dump(layers_random, open(f"outputs/repeat/streamlit/layers_loss_random_n.json", "w"))
+    json.dump(layers_pattern, open(f"outputs/repeat/streamlit/layers_loss_pattern_n.json", "w"))
+    json.dump(layers_top100, open(f"outputs/repeat/streamlit/layers_loss_top100_n.json", "w"))
+    json.dump(layers_bottom100, open(f"outputs/repeat/streamlit/layers_loss_bottom100_n.json", "w"))
 
   elif FLAGS.experiment == "run_clustering_analysis":
     cluster_groups, sample_vectors, sample_sequences, cluster_labels = clustering_analysis(FLAGS.num_trials, FLAGS.seq_length, FLAGS.seq_length - 1, 

src/experiments/utils/curvature_utils.py
@@ -4,7 +4,6 @@ from transformer_lens.hook_points import (
     HookedRootModule,
     HookPoint,
 )  # Hooking utilities
-
 from transformer_lens import HookedTransformer, HookedTransformerConfig, FactoredMatrix, ActivationCache
 import torch
 from fancy_einsum import einsum
@@ -107,24 +106,65 @@ def layer_wise_curvature(cache, total_layers, stream_idx, sequence_length, inclu
     curvatures.append(dict_distance)
   return curvatures
 
-def layer_wise_norm_running_average(cache, total_layers, stream_idx, sequence_length, include_mlps=True, include_attn=True, cumulative=False):
+def layer_wise_norm_running_average(cache, total_layers, stream_idx, sequence_length, include_mlps=True, include_attn=True):
   norm_running_avgs = []
+  rol_avg_dict_norm = {layer: 0 for layer in range(total_layers)}
+  count = 0
   for str_idx in range(stream_idx, len(cache["resid_pre", 0]), sequence_length):
-    
-    rol_avg_dict_norm = {}
-    count = 0
+    count += 1
     for layer in range(total_layers):
       distance = 0.0
-      
       if include_mlps:
         distance+=torch.linalg.norm(cache['mlp_out', layer][str_idx])
       if include_attn:
         distance+=torch.linalg.norm(cache['attn_out', layer][str_idx])
-      count += 1
+      rol_avg_dict_norm[layer] = (rol_avg_dict_norm[layer]*(count - 1) +(distance).clone().detach().item())/count 
+    norm_running_avgs.append(rol_avg_dict_norm.copy())
     
-      rol_avg_dict_norm[layer] = (rol_avg_dict_norm[layer]*(count - 1) +(distance).clone().detach().item())/count
-    norm_running_avgs.append(rol_avg_dict_norm)
   return norm_running_avgs
+
+def layer_wise_consec_kl_divs(model, cache, total_layers, stream_idx, sequence_length, resid_post=False, include_mlps=True, include_attn=True):
+  consec_kl_divs = []
+  
+  for str_idx in range(stream_idx, len(cache["resid_pre", 0]) - sequence_length, sequence_length):
+    ## for loop goes all the way to one from the end because we are comparing the ith and i+1th token predictions
+    kl_div_curr = {}
+    for layer in range(total_layers):
+      layer_distr, layer_distr_n = 0.0, 0.0
+      if not resid_post:
+        if include_mlps:
+          layer_distr += cache['mlp_out', layer][str_idx]
+          layer_distr_n += cache['mlp_out', layer][str_idx + sequence_length]
+        if include_attn:
+          layer_distr += cache['attn_out', layer][str_idx]
+          layer_distr_n += cache['mlp_out', layer][str_idx + sequence_length]
+      
+      if resid_post:
+        layer_distr += cache['resid_post', layer][str_idx]
+        layer_distr_n += cache['resid_post', layer][str_idx + sequence_length]
+      # print(layer_distr.unsqueeze([0, 1]).shape)
+      logits_distr = model.unembed(layer_distr.view(1, 1, -1))
+      logits_distr_n = model.unembed(layer_distr_n.view(1, 1, -1))
+      # umembed expects batch_size x sequence_length x embedding_dim
+      
+      log_probs = torch.nn.functional.log_softmax(logits_distr, dim=-1).view(-1)
+      log_probs_n = torch.nn.functional.log_softmax(logits_distr_n, dim=-1).view(-1)
+      # convert back to 1 dimensional vector
+      
+      kl_div_curr[layer] = torch.nn.functional.kl_div(log_probs, log_probs_n, log_target=True).detach().item()
+          
+    consec_kl_divs.append(kl_div_curr.copy())
+  
+  ## one extra just so it is the same length as the other lists
+  consec_kl_divs.append(kl_div_curr.copy())
+  return consec_kl_divs
+
+def compute_prob_kls(prob_dists):
+  '''Computes the kl distribution between the probability distributions of the ith and i+1th token predictions'''
+  kls = []
+  for i in range(len(prob_dists) - 1):
+    kls.append(torch.nn.functional.kl_div(prob_dists[i+1], prob_dists[i], log_target=True).cpu().numpy())
+  return kls
 
 def logits_to_ave_logit_diff(logits, answer_tokens, per_prompt=False, cumulative=False):
   # Only the final logits are relevant for the answer

src/streamlit_app.py
@@ -44,59 +44,59 @@ def intro(offset=0):
     The number of repeated sequences is varied from 1 to 100. We use [transformer-lens](https://github.com/neelnanda-io/TransformerLens)
     """)
 
+# -------------------------------------  ORIGINAL REPEATED SEQ FUNCTIONS --------------------------------------
 def random_sequences(offset=(0, 100)):
   st.write("## Random Sequences")
-  st.write("### Mean Layer-Wise Curvature Random Sequences")
-  dict_mean_layers = compute_mean_layers('outputs/repeat/streamlit/layers_loss_random.json')
+  option = st.selectbox(
+    'Would you like to visualize curvature or running norm?',
+    ('curvature', 'norm'))
+  if option == 'norm':
+    st.write("### Mean Layer-Wise Running Norm Random Sequences")
+    dict_mean_layers = compute_mean_layers('outputs/repeat/streamlit/layers_loss_random_n.json')
+  else:
+    st.write("### Mean Layer-Wise Curvature Random Sequences")
+    dict_mean_layers = compute_mean_layers('outputs/repeat/streamlit/layers_loss_random.json')
   plot_layer_curvature_loss_vs_repetitions(dict_mean_layers, offset)
   
 def patterned_sequences(offset=(0, 100)):
   st.write("## Patterned Sequences")
-  st.write("### Mean Layer-Wise Curvature Patterned Sequences")
-  dict_mean_layers = compute_mean_layers('outputs/repeat/streamlit/layers_loss_pattern.json')
+  option = st.selectbox(
+    'Would you like to visualize curvature or running norm?',
+    ('curvature', 'norm'))
+  if option == 'norm':
+    st.write("### Mean Layer-Wise Running Norm Patterned Sequences")
+    dict_mean_layers = compute_mean_layers('outputs/repeat/streamlit/layers_loss_pattern_n.json')
+  else:
+    st.write("### Mean Layer-Wise Curvature Patterned Sequences")
+    dict_mean_layers = compute_mean_layers('outputs/repeat/streamlit/layers_loss_pattern.json')
   plot_layer_curvature_loss_vs_repetitions(dict_mean_layers, offset)
 
 def top100_sequences(offset=(0, 100)):
   st.write("## Top 100 Sequences")
-  st.write("### Mean Layer-Wise Curvature Top 100 Sequences")
-  
-  dict_mean_layers = compute_mean_layers('outputs/repeat/streamlit/layers_loss_top100.json')
+  option = st.selectbox(
+    'Would you like to visualize curvature or running norm?',
+    ('curvature', 'norm'))
+  if option == 'norm':
+    st.write("### Mean Layer-Wise Running Norm Top 100 Sequences")
+    dict_mean_layers = compute_mean_layers('outputs/repeat/streamlit/layers_loss_top100_n.json')
+  else:
+    st.write("### Mean Layer-Wise Curvature Top 100 Sequences")
+    dict_mean_layers = compute_mean_layers('outputs/repeat/streamlit/layers_loss_top100.json')
   plot_layer_curvature_loss_vs_repetitions(dict_mean_layers, offset)
 
 def bottom100_sequences(offset=(0, 100)):
   st.write("## Bottom 100 Sequences")
-  st.write("### Mean Layer-Wise Curvature Bottom 100 Sequences")
-  dict_mean_layers = compute_mean_layers('outputs/repeat/streamlit/layers_loss_bottom100.json')
+  option = st.selectbox(
+    'Would you like to visualize curvature or running norm?',
+    ('curvature', 'norm'))
+  if option == 'norm':
+    st.write("### Mean Layer-Wise Running Norm Bottom 100 Sequences")
+    dict_mean_layers = compute_mean_layers('outputs/repeat/streamlit/layers_loss_bottom100_n.json')
+  else:
+    st.write("### Mean Layer-Wise Curvature Bottom 100 Sequences")
+    dict_mean_layers = compute_mean_layers('outputs/repeat/streamlit/layers_loss_bottom100.json')
   plot_layer_curvature_loss_vs_repetitions(dict_mean_layers, offset)
-  
-def plot_layer_curvature_loss_vs_repetitions(dict_mean_layers, offset=(0, 100)):
-  keys = list(dict_mean_layers.keys())
-  keys.remove('loss')
-  x = list(range(len(dict_mean_layers['loss'])))
-
-  for key in keys:
-    fig, ax1 = plt.subplots()
-    ax1.scatter(x[offset[0]:offset[1]], dict_mean_layers[key][offset[0]:offset[1]], label=f'curvature_layer_{key}', color='b')
-    ax1.set_xlabel('Repetitions')
-    ax1.set_ylabel(f'layer {key} curvature', color='b')
-    ax1.tick_params(axis='y', labelcolor='b')
-
-    ax2 = ax1.twinx()
     
-    ax2.scatter(x[offset[0]:offset[1]], dict_mean_layers['loss'][offset[0]:offset[1]], label='loss', color='r')
-    ax2.set_ylabel('loss', color='r')
-    ax2.tick_params(axis='y', labelcolor='r')
-      
-    lines1, labels1 = ax1.get_legend_handles_labels()
-    lines2, labels2 = ax2.get_legend_handles_labels()
-      
-    lines =  lines1 + lines2
-    labels =  labels1 + labels2
-    ax1.legend(lines, labels, loc='upper right')
-    
-    plt.title(f"Layer {key}")
-    st.pyplot(fig)
-
 def logit_lens(offset=(0, 100)):
   st.write("## Logit Lens Analysis")
   option = st.selectbox(
@@ -109,10 +109,12 @@ def logit_lens(offset=(0, 100)):
     results = logit_lens_analysis(40, tokenizer, i, seq_length,
                                   model, device="cpu", how=how)
     answer_residual_directions = model.tokens_to_residual_directions(results['answer_tokens'])
+    ## num_samples decoding vectors
     logit_diff_directions = answer_residual_directions[:, 0] - answer_residual_directions[:, 1]
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
     accumulated_residual, labels = results['cache'].accumulated_resid(layer=-1, incl_mid=True, pos_slice=-1, return_labels=True)
     logit_lens_logit_diffs = residual_stack_to_logit_diff(accumulated_residual, results['cache'], logit_diff_directions, results['prompts'])
+    
     ax1.plot(np.arange(model.cfg.n_layers*2+1)/2, tutils.to_numpy(logit_lens_logit_diffs))
     ax1.set_xticks(np.arange(model.cfg.n_layers*2+1)/2)
     ax1.set_xticklabels(labels, rotation=90)
@@ -178,6 +180,19 @@ def clustering(offset=(0, 100)):
       )
   st.plotly_chart(fig, use_container_width=True)
 
+# ------------------------- DATA READING AND PROCESSING -----------------------------
+
+@st.cache_data
+def compute_mean_layers_lookup(file):
+  layer_dict = json.load(open(file, "r"))
+  for key, value in layer_dict.items():
+    if key == "loss":
+      layer_dict[key] = np.mean(value, axis=0)
+    else:
+      layer_dict[key]["running_norm"] = np.mean(value["running_norm"], axis=0)
+      layer_dict[key]["consec_kls"] = np.mean(value["consec_kls"], axis=0)
+  return layer_dict
+
 @st.cache_data
 def compute_mean_layers(file):
   layer_dict = json.load(open(file, "r"))
@@ -185,14 +200,134 @@ def compute_mean_layers(file):
     layer_dict[key] = np.mean(value, axis=0)
   return layer_dict
 
+# ----------------------------------- LOOKUP SEQUENCES -------------------------------------
+
+def lookup_sequences(offset=(0, 100)):
+  st.write("## Lookup Task Sequences")
+  option = st.selectbox(
+    'Would you like to visualize mlp/attn outs or resid post?',
+    ('mlp/attn out', 'resid post'))
+  params = st.selectbox(
+    'Which set of parameters would you like to use?',
+    ('Rand Seq Len = 0, Key Len = 4, Dict Size = 1', 'Rand Seq Len = 3, Key Len = 1, Dict Size = 1', 'Rand Seq Len = 3, Key Len = 1, Dict Size = 2', 
+     'Rand Seq Len = 3, Key Len = 1, Dict Size = 3'))
+  st.write("### Mean Layer-Wise Metrics Among 50 Examples")
+  if params == 'Rand Seq Len = 0, Key Len = 4, Dict Size = 1':
+    if option == 'mlp/attn out':
+      dict_mean_layers = compute_mean_layers_lookup(f'outputs/lookup/streamlit/layer_wise_analysis_50_outs_cont.json')
+    else:
+      dict_mean_layers = compute_mean_layers_lookup(f'outputs/lookup/streamlit/layer_wise_analysis_50_resids_cont.json')
+  elif params == "Rand Seq Len = 3, Key Len = 1, Dict Size = 1":
+    if option == 'mlp/attn out':
+      dict_mean_layers = compute_mean_layers_lookup(f'outputs/lookup/streamlit/layer_wise_analysis_50_outs.json')
+    else:
+      dict_mean_layers = compute_mean_layers_lookup(f'outputs/lookup/streamlit/layer_wise_analysis_50_resids.json')
+  elif params == "Rand Seq Len = 3, Key Len = 1, Dict Size = 2":
+    if option == 'mlp/attn out':
+      dict_mean_layers = compute_mean_layers_lookup(f'outputs/lookup/streamlit/layer_wise_analysis_50_outs_sd_2.json')
+    else:
+      dict_mean_layers = compute_mean_layers_lookup(f'outputs/lookup/streamlit/layer_wise_analysis_50_resids_sd_2.json')
+  else:
+    if option == 'mlp/attn out':
+      dict_mean_layers = compute_mean_layers_lookup(f'outputs/lookup/streamlit/layer_wise_analysis_50_outs_sd_3.json')
+    else:
+      dict_mean_layers = compute_mean_layers_lookup(f'outputs/lookup/streamlit/layer_wise_analysis_50_resids_sd_3.json')
+  
+  plot_layer_lookup_metrics(dict_mean_layers, offset)
+
+
+# -------------------------------- ORGINIZATIONAL FUNCTIONS ---------------------------------------
+def repeated_sequences(offset=0):
+  page_names_to_funcs = {
+    "Random Sequences": random_sequences,
+    "Patterned Sequences": patterned_sequences,
+    "Top 100 Sequences": top100_sequences,
+    "Bottom 100 Sequences": bottom100_sequences,
+    "Logit Lens Analysis": logit_lens,
+    "Clustering": clustering,
+    }
+  demo_name = st.selectbox("Select a repeated sequence visualization", list(page_names_to_funcs.keys()))
+  page_names_to_funcs[demo_name](offset)
+
+def lookup_task(offset=0):
+  page_names_to_funcs = {
+    "Layerwise Analysis": lookup_sequences,
+    # "Logit Lens Analysis": logit_lens,
+    }
+  demo_name = st.selectbox("Select a lookup sequence visualization", list(page_names_to_funcs.keys()))
+  page_names_to_funcs[demo_name](offset)
+  
+# ----------------------------------- PLOTTING FUNCTIONS -------------------------------------
+def plot_layer_lookup_metrics(dict_mean_layers, offset=(0, 100)):
+  keys = list(dict_mean_layers.keys())
+  keys.remove('loss')
+  x = list(range(len(dict_mean_layers['loss'])))
+
+  for key in keys:
+    fig, ax1 = plt.subplots()
+    ax1.scatter(x[offset[0]:offset[1]], dict_mean_layers[key]['running_norm'][offset[0]:offset[1]], label=f'running_norm_layer_{key}', color='b')
+    ax1.set_xlabel('Repetitions')
+    ax1.set_ylabel(f'layer {key} running norm', color='b')
+    ax1.tick_params(axis='y', labelcolor='b')
+    
+    ax3 = ax1.twinx()
+    
+    ax3.scatter(x[offset[0]:offset[1]], dict_mean_layers['loss'][offset[0]:offset[1]], label='loss', color='r')
+    ax3.set_ylabel('loss', color='r')
+    ax3.tick_params(axis='y', labelcolor='r')
+
+    ax2 = ax1.twinx()
+    
+    ax2.scatter(x[offset[0]:offset[1]], dict_mean_layers[key]['consec_kls'][offset[0]:offset[1]], label=f'consec_kls_layer_{key}', color='g')
+    ax2.set_ylabel(f'layer {key} kl div', color='g')
+    ax2.tick_params(axis='y', labelcolor='g')
+    
+      
+    lines1, labels1 = ax1.get_legend_handles_labels()
+    lines3, labels3 = ax3.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    
+    lines =  lines1 + lines2 + lines3
+    labels =  labels1 + labels2 + labels3
+    
+    ax1.legend(lines, labels, loc='upper right')
+    
+    plt.title(f"Layer {key}")
+    st.pyplot(fig)
+
+def plot_layer_curvature_loss_vs_repetitions(dict_mean_layers, offset=(0, 100), curvature=True):
+  keys = list(dict_mean_layers.keys())
+  keys.remove('loss')
+  x = list(range(len(dict_mean_layers['loss'])))
+  prefix = "curvature" if curvature else "running_norm"
+    
+  for key in keys:
+    fig, ax1 = plt.subplots()
+    ax1.scatter(x[offset[0]:offset[1]], dict_mean_layers[key][offset[0]:offset[1]], label=f'{prefix}_layer_{key}', color='b')
+    ax1.set_xlabel('Repetitions')
+    ax1.set_ylabel(f'layer {key} {prefix}', color='b')
+    ax1.tick_params(axis='y', labelcolor='b')
+    
+    ax2 = ax1.twinx()
+    
+    ax2.scatter(x[offset[0]:offset[1]], dict_mean_layers['loss'][offset[0]:offset[1]], label='loss', color='r')
+    ax2.set_ylabel('loss', color='r')
+    ax2.tick_params(axis='y', labelcolor='r')
+      
+    lines1, labels1 = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+      
+    lines =  lines1 + lines2
+    labels =  labels1 + labels2
+    ax1.legend(lines, labels, loc='upper right')
+    
+    plt.title(f"Layer {key}")
+    st.pyplot(fig)
+    
 page_names_to_funcs = {
   "Introduction": intro,
-  "Random Sequences": random_sequences,
-  "Patterned Sequences": patterned_sequences,
-  "Top 100 Sequences": top100_sequences,
-  "Bottom 100 Sequences": bottom100_sequences,
-  "Logit Lens Analysis": logit_lens,
-  "Clustering": clustering,
+  "Repeated Sequences": repeated_sequences, 
+  "Lookup Task": lookup_task
 }
 
 demo_name = st.sidebar.selectbox("Select a visualization", list(page_names_to_funcs.keys()))